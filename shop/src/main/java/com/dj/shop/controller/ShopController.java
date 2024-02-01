0b71bc786806d34a9f1ef06960fcf6a3fbaab89b
antan <antan@DESKTOP-6657P6A>
2024-02-01 15:59:31 +0900

    무료 템플릿 적용

---
+++ shop/src/main/java/com/dj/shop/controller/ShopController.java
@@ -0,0 +1,11 @@
+package com.dj.shop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class ShopController {
+	@GetMapping("/")
+	public String main() {
+		return "pages/index";
+	}
+}
