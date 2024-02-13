6b7abccb2746c5755a0fde3856d346ea589ff77e
antan <antan@59.23.116.187>
2024-02-13 21:06:02 +0900

    회원가입 유효성 검사 추가

--- shop/src/main/java/com/dj/shop/controller/UserController.java
+++ shop/src/main/java/com/dj/shop/controller/UserController.java
@@ -3,7 +3,6 @@ package com.dj.shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
