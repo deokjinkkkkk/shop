8d8360f676ad94be1d719ed3332b544997624dcc
Deok <antan3957@naver.com>
2024-05-23 17:52:58 +0900

    css 정리 및 리뷰

--- shop/src/main/java/com/dj/shop/controller/ReviewController.java
+++ shop/src/main/java/com/dj/shop/controller/ReviewController.java
@@ -2,10 +2,18 @@ package com.dj.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
 	
+	@GetMapping("/list")
+	public String reviewForm() {
+
+		return "pages/review/reviewlist";
+	}
 	
 }
