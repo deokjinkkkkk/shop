0facafb93de012b87b421f05e96d8b72acb32235
Deok <antan3957@naver.com>
2024-06-17 14:25:18 +0900

    코드 정리

--- shop/src/main/resources/static/assets/pages/scripts/productDetail.js
+++ shop/src/main/resources/static/assets/pages/scripts/productDetail.js
@@ -1,81 +1,78 @@
 $(document).ready(function(){
-
     reviewlist();
-});
-
-
-$("#orderBtn").click(function(){
-	let proCount = $("#cnt").val();
-    $("#orderForm").find("input[name='orders[0].orderCnt']").val(proCount);
-    $("#orderForm").submit();
-});
-
-$("#updateBtn").click(function(){
-	let productNum = $("input[name='productNum']").val();
-    location.href = "/product/updateForm?productNum=" + productNum;
-});
-
-$("#deleteBtn").click(function(){
-    $("#cartInsert").attr("action", "/product/productDelete").submit();
-});
-
-$("#cartBtn").click(function(e){
-    var cnt = $("#cnt").val();Q
-    console.log(cnt);
-    var productNumber = $("#productNum").val();
-    $.ajax({
-        url: '/cart/add',
-        type: 'POST',
-        data: {
-            cnt: cnt,
-            productNum: productNumber
-        },
-        success: function(result){
-            if(result == 0){
-                alert("장바구니에 추가를 하지 못하였습니다.");
-            } else if(result == 1){
-                alert("장바구니에 추가 하였습니다.");
-            } else if(result == 2){
-                alert("장바구니에 이미 상품이 있습니다.");
-            } else if(result == 3){
-                alert("로그인이 필요 합니다.");
-            } else if(result == 4){
-                alert("상품의 재고가 부족합니다.");
-            }
-        }
+    $("#orderBtn").off('click').on('click', function(){
+        let proCount = $("#cnt").val();
+        $("#orderForm").find("input[name='orders[0].orderCnt']").val(proCount);
+        $("#orderForm").submit();
     });
-});
 
-$("#review_button").click(function(){
-	let productNum = $("input[name='productNum']").val();
-    let review = $("#reviewText").val();
-    let revStar = $("input[name='rating']:checked").val();
-    if (review == "") {
-        alert("내용을 작성해 주세요");
-        return;
-    }
-    if(revStar == undefined){
-        alert("별점을 입력해주세요");
-        return;
-    }
-    $.ajax({
-        url: "/review/add",
-        type: "POST",
-        data: {
-            revContent: review,
-            revStar: revStar,
-            productNum: productNum
-        },
-        success: function(result){
-            if (result == 1) {
-                alert("등록완료");
-                reviewlist();
-            } else if(result == 2){
-                alert("로그인후에 댓글등록이 가능합니다.");
-            } else if (result == 3) {
-                alert("리뷰를 작성한 상품 입니다.");
+    $("#updateBtn").off('click').on('click', function(){
+        let productNum = $("input[name='productNum']").val();
+        location.href = "/product/updateForm?productNum=" + productNum;
+    });
+
+    $("#deleteBtn").off('click').on('click', function(){
+        $("#cartInsert").attr("action", "/product/productDelete").submit();
+    });
+
+    $("#cartBtn").off('click').on('click', function(e){
+        console.log("어");
+        var cnt = $("#cnt").val();
+        var productNumber = $("#productNum").val();
+        $.ajax({
+            url: '/cart/add',
+            type: 'POST',
+            data: {
+                cnt: cnt,
+                productNum: productNumber
+            },
+            success: function(result){
+                if(result == 0){
+                    alert("장바구니에 추가를 하지 못하였습니다.");
+                } else if(result == 1){
+                    alert("장바구니에 추가 하였습니다.");
+                } else if(result == 2){
+                    alert("장바구니에 이미 상품이 있습니다.");
+                } else if(result == 3){
+                    alert("로그인이 필요 합니다.");
+                } else if(result == 4){
+                    alert("상품의 재고가 부족합니다.");
+                }
             }
+        });
+    });
+
+    $("#review_button").off('click').on('click', function(){
+        let productNum = $("input[name='productNum']").val();
+        let review = $("#reviewText").val();
+        let revStar = $("input[name='rating']:checked").val();
+        if (review == "") {
+            alert("내용을 작성해 주세요");
+            return;
         }
+        if(revStar == undefined){
+            alert("별점을 입력해주세요");
+            return;
+        }
+        $.ajax({
+            url: "/review/add",
+            type: "POST",
+            data: {
+                revContent: review,
+                revStar: revStar,
+                productNum: productNum
+            },
+            success: function(result){
+                if (result == 1) {
+                    alert("등록완료");
+                    reviewlist();
+                } else if(result == 2){
+                    alert("로그인후에 댓글등록이 가능합니다.");
+                } else if (result == 3) {
+                    alert("리뷰를 작성한 상품 입니다.");
+                }
+            }
+        });
     });
 });
 
@@ -116,11 +113,10 @@ function reviewlist(){
 
 document.addEventListener('DOMContentLoaded', (event) => {
     let productNum = $("input[name='productNum']").val();
-    checkWishlistStatus(productNum);  // 초기 로드 시 상품 ID 1의 위시리스트 상태 확인
+    checkWishlistStatus(productNum);
 });
 
 function checkWishlistStatus(productNum) {
-    // 서버에 현재 상품의 위시리스트 상태를 요청합니다.
     fetch(`/myPage/checkWishiList?productNum=${productNum}`)
         .then(response => response.json())
         .then(data => {
